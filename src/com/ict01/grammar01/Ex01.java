d4d0c4f1c41026c927a4d9bbdada5643ffe11270
zerohoons <zerohoons@gmail.com>
2021-04-05 13:22:21 +0900

    이클립스 깃 연습버전.

--- src/com/ict01/grammar01/Ex01.java
+++ src/com/ict01/grammar01/Ex01.java
@@ -2,5 +2,9 @@ package com.ict01.grammar01;
 class Ex01{
 	public static void main(String[] args){
 		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
 	}
 }
--- src/com/ict03/class01/Ex13.java
+++ src/com/ict03/class01/Ex13.java
@@ -11,6 +11,15 @@ public class Ex13 {
 	public Ex13() {
 		/* this : 객체 내부에서 객체 자신을 지칭하는 예약어
 		 * this.변수 : 전역변수
-		 */
+		 * this.() : 생성자가 생성자를 호출할 때 사용하는 예약어. 반드시 생성자 첫줄에만 사용가능*/
+		//this("공실이", 21, "남극");
+		System.out.println("기본생성자 : " + this);
+		//System.out.println(this.name);
+		//System.out.println(this.getName());
+	}
+	
+	//source 메뉴
+	public Ex13(String name, int age) {
+		
 	}
 }
