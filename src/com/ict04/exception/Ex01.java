2506e94e7d97aaf235f2d2ab47e2c599c9a137d0
zerohoons <zerohoons@gmail.com>
2021-04-05 16:18:13 +0900

    exception 예외설정 + 자잘한 오류 수정

--- src/com/ict04/exception/Ex01.java
+++ src/com/ict04/exception/Ex01.java
@@ -6,7 +6,29 @@ public class Ex01 {
 		 * 예상된 오류나 가벼운 오류의 경우, 별도로 처리하는 방법을 사용하면
 		 * 프로그램이 강제로 종료되는 것이 아닌, 정상 종료를 하게 된다. 이러한 방법을 예외처리라고 한다.*/
 		
-		//예외처리. try~ chatch~ 
-		
+		//try ~ catch 문
+		/*try{
+		 * 예외가 발생 가능한 문장} catch(예상되는 예외 객체 변수명{
+		 * 해당 예외가 발생시 수행할 문장. }
+		 * 예외가 발생안할경우 지나감.
+		 * 
+		 * 다중 캐치문
+		 * try{
+		 * }catch(1){
+		 * }catch(2){
+		 * }catch(3){ }*/
+		try {
+		int [] ver = { 10, 20, 30};
+		for (int i = 0; i <= ver.length; i++) {
+			System.out.println(1);
+			System.out.println(1);
+			System.out.println("ver[" + i + "] = "+ver[i]);
+			System.out.println(3);
+			System.out.println(4);
+		} } catch(Exception e) {
+
+		}
+		System.out.println("수고하셨습니다.");
+	
 	}
 }
---
+++ src/com/ict04/exception/Ex02.java
@@ -0,0 +1,15 @@
+package com.ict04.exception;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		try {
+		String year = "2002년";
+		int age = 2012 - Integer.parseInt(year);
+		System.out.println("나이 : "+age);
+		}catch (NumberFormatException e) {
+			System.out.println("숫자만 입력하세요.");
+		}
+		System.out.println("수고하셨습니다.");
+	
+		}
+}
